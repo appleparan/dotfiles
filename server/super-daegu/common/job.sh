f1c8636fdf96a5aea8985c78e342916e334ed65f
Liam Jongsu Kim <jskim.cfd@gmail.com>
2014-04-10 22:56:54 +0900

    adpot sample job script

--- server/super-daegu/common/job.sh
+++ server/super-daegu/common/job.sh
@@ -1,32 +1,17 @@
-#!/bin/sh
-#### Requesting job resources
-### PBS -l select=[nodes]:ncpus=[cpus]:ngpus=[gpus]
-#PBS -l select=8:ncpus=16:ngpus=1
-
-#### Setting email recipient list
-#PBS -M Enter Email Address
-
-#### mail option(Specifying email notification)
-### a send mail when job is aborted by batch system
-### b send mail when job begins execution
-### e send mail when job ends execution
-### n do not send mail
-#PBS -m abe
-
-#### Specifying queue and/or server
+#!/bin/bash
+#PBS -l select=1:ncpus=16:ngpus=1
+#PBS -M Mail Address
+### mail option
+## a send mail when job is aborted by batch system
+## b send mail when job begins execution
+## e send mail when job ends execution
+## n do not send mail
+#PBS -m n
+#PBS -N test
 #PBS -q workq
-
-#### Marking a job as re-runnable or not
 #PBS -r n
-
-#### Exporting environment variables
-#PBS -V
-
-#### Specifying a job name
-### PBS -N [jobname]
-#PBS -N 8node-16
-
-#### Create Stdout.out File 
+#PBS -j oe
+export OMP_NUM_THREADS=16
 cd $PBS_O_WORKDIR
 date > stdout.out
 cat $PBS_NODEFILE >> stdout.out
@@ -36,4 +21,6 @@ NP=`/usr/bin/wc -l $PBS_NODEFILE | awk '{ print $1 }'`
 echo $NP >> stdout.out
 
 #### mpirun config
-mpirun -genv I_MPI_FABRICS=shm:ofa -genv I_MPI_OFA_USE_XRC=1 -genv I_MPI_OFA_DYNAMIC_QPS=1 -genv I_MPI_DEBUG=5 -np $NP -machinefile $PBS_NODEFILE /hello.out >> stdout.out
+###mpirun -genv I_MPI_FABRICS=shm:ofa -genv I_MPI_OFA_USE_XRC=1 -genv I_MPI_OFA_DYNAMIC_QPS=1 -genv I_MPI_DEBUG=5 -np $NP -machinefile $PBS_NODEFILE /a.out >> stdout.out
+
+./a.out >> stdout.out
