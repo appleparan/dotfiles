b055584fbef8c368e7aaefb65753410ff4709fc1
Liam Jongsu Kim <jskim.cfd@gmail.com>
2017-02-09 15:20:55 +0900

    add 16core settings for openmp (use GPU)

---
+++ servercfg/dgist/public/openmp-16core.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#PBS -l select=1:ncpus=16:ngpus=1
+#PBS -M Mail Address
+### mail option
+## a send mail when job is aborted by batch system
+## b send mail when job begins execution
+## e send mail when job ends execution
+## n do not send mail
+#PBS -m n
+#PBS -N test
+#PBS -q workq
+#PBS -r n
+export OMP_NUM_THREADS=16
+cd $PBS_O_WORKDIR
+[ -f jobout.out ] && rm jobout.out
+date > jobout.out
+cat $PBS_NODEFILE >> jobout.out
+
+NP=`/usr/bin/wc -l $PBS_NODEFILE | awk '{ print $1 }'`
+
+echo $NP >> jobout.out
+
+./a.out >> jobout.out 2>&1
