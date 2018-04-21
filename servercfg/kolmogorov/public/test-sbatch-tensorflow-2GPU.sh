3cd46e723224c57278e83b41776f367f77a75fd8
Jongsu Liam Kim <jongsukim8@gmail.com>
2018-04-21 14:13:22 +0900

    add kolmogorov server configuration files

---
+++ servercfg/kolmogorov/public/test-sbatch-tensorflow-2GPU.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH -J  tensor-2GPU                   # Job name
+#SBATCH -o  out.test-tensorflow-2GPU.%j   # Name of stdout output file (%j expands to %jobId)
+#SBATCH -p  gpu                           # queue or partiton name
+#SBATCH --gres=gpu:2                      # Num Devices
+
+echo
+python3 -V
+which python3
+which pip3
+echo
+pip3 list | grep tensorflow
+echo
+python3 "your_python_file.py"
+
+# End of File.
