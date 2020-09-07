d17371994ef9f1e4cf82307292e177e192dcb158
Jongsu Liam Kim <jongsukim8@gmail.com>
2020-09-07 10:11:49 +0900

    Update comment

--- servercfg/kolmogorov/public/test-openmp.sh
+++ servercfg/kolmogorov/public/test-openmp.sh
@@ -3,9 +3,9 @@
 # checkout https://slurm.schedmd.com/sbatch.html for more details
 #SBATCH -J openmp-jobname           # Job name
 #SBATCH --nodes=1                   # Total Number of Nodes
-#SBATCH --ntasks=1                  # Number of "tasks". For use with distributed parallelism. 
-#SBATCH --cpus-per-task=16          # # of CPUs allocated to each task. For use with shared memory parallelism.
-#SBATCH --ntasks-per-node=1         # Number of "tasks" per node. For use with distributed parallelism. 
+#SBATCH --ntasks=1                  # Number of "tasks". For use with distributed parallelism (i.e. MPI) 
+#SBATCH --cpus-per-task=16          # # of CPUs allocated to each task. For use with shared memory parallelism. (i.e. OpenMP)
+#SBATCH --ntasks-per-node=1         # Number of "tasks" per node. For use with distributed parallelism (i.e. MPI)
 #SBATCH -o out.%j.%x                # Name of stdout output file (%j - job id, %x - job name)
 #SBATCH -p cpu                      # queue or partiton name
 
